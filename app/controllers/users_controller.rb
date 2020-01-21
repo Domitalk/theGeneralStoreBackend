6f9bcae04dc68e2a1ce4f6789cfcd513c44dbb10
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-21 15:20:55 -0500

    maybe done with auth?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+    # def create 
+    #     @user = User.create(user_params)
+    #     if @user.valid? 
+    #         render json: { user: UserSerializer.new(@user) }, status: :created 
+    #     else  
+    #         render json: { error: 'failed to create user' }, status: :not_acceptable 
+    #     end 
+    # end 
+
     def show 
         @user = User.find(params[:id])
         @past_carts = @user.carts 
@@ -9,8 +18,10 @@ class UsersController < ApplicationController
     def login 
         @users = User.all 
         render json: @users.to_json
-
     end 
-    
 
+    # private 
+    # def user_params 
+    #     params.require(:user).permit(:name, :password)
+    # end 
 end
