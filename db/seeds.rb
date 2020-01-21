945d93fa4788f3463621b320bcf32dd18e8fea49
Imade Osifo <toxsik88@gmail.com>
2020-01-21 15:18:10 -0500

    Capitalized seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,31 @@ u2 = User.create({name: "imade"})
 u3 = User.create({name: "ignacious pennybottom"})
 
 i1 = Item.create({
-    name: "artisanal ice",
+    name: "Artisanal Ice",
     price: 5.00, 
     picture: "https://img.grouponcdn.com/seocms/kkzUERq83gBf4e5f5zNhmi/Just_Ice_hero_600x390-600x390.jpeg"
 })
 
 i2 = Item.create({
-    name: 'vintage hip sachel', 
+    name: 'Vintage Hip Sachel', 
     price: 100.00, 
     picture: "https://images-na.ssl-images-amazon.com/images/I/61enMjomB5L._SY550_.jpg"
 })
 
 i3 = Item.create({
-    name: "mustache wax", 
+    name: "Mustache Wax", 
     price: 20.00,
     picture: "https://images-na.ssl-images-amazon.com/images/I/71gam7mXv%2BL._SY355_.jpg"
 })
 
 i4 = Item.create({
-    name: "seasonal perserves recepticle",
+    name: "Seasonal Perserves Recepticle",
     price: 10.00,
     picture: "https://target.scene7.com/is/image/Target/GUEST_6da2d695-d402-40d2-8964-f3d6fdd150b9?wid=488&hei=488&fmt=pjpeg"
 })
 
 i5 = Item.create({
-    name: "authentic puritan butter churn",
+    name: "Authentic Puritan Butter Churn",
     price: 150.00,
     picture: "https://cdn.shopify.com/s/files/1/0050/1275/0447/products/bc-870_6097c58c-f112-4591-885a-e1e015df80f3_shopify_800x.jpg?v=1571714803"
 })
@@ -50,25 +50,25 @@ i6 = Item.create({
 })
 
 i7 = Item.create({
-    name: "turn of the century penny-farthing bicycle",
+    name: "Turn of the Century Penny-Farthing Bicycle",
     price: 300.00,
     picture: "https://i.pinimg.com/originals/65/62/33/656233f4f324f769a66a7dd8b4d3b5e2.jpg"
 })
 
 i8 = Item.create({
-    name: "reclaimed wood coffee table",
+    name: "Reclaimed Wood Coffee Table",
     price: 200.00,
     picture: "https://i.etsystatic.com/9532249/r/il/997d84/1848990515/il_570xN.1848990515_s6fj.jpg"
 })
 
 i9 = Item.create({
-    name: "egyptian canonical sundial",
+    name: "Egyptian Canonical Sundial",
     price: 150.00,
     picture: "https://i.ebayimg.com/images/g/IYUAAOSwHLNZPsfU/s-l400.jpg"
 })
 
 i10 = Item.create({
-    name: "leather warrior hair wrap for the gentlemen's manbun",
+    name: "Leather Warrior Hair Wrap for the Gentlemen's Manbun",
     price: 20.00,
     picture: "https://i.etsystatic.com/12796248/r/il/dca346/1645628813/il_794xN.1645628813_kstd.jpg"
 })
