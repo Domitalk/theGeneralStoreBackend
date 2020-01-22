656fabc10c0cacf205b3b8c5f8a91b60627ee845
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-22 11:29:38 -0500

    morn

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -25,7 +25,7 @@ class Api::V1::UsersController < ApplicationController
     private 
     
     def user_params 
-        params.require(:user).permit(:name, :password) 
+        params.require(:user).permit(:name, :password, :picture) 
     end 
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name
+  attributes :name, :picture
 end
